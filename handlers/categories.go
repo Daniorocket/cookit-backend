4742b471fc3719e171740caa431c33b99faf80cc
Daniorocket <daniorocket@gmail.com>
2021-07-20 15:16:01 +0200

    Refactor decode multipart request http

--- handlers/categories.go
+++ handlers/categories.go
@@ -3,19 +3,20 @@ package handlers
 import (
 	"bytes"
 	"encoding/base64"
-	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"path"
 	"strconv"
 
+	"github.com/Daniorocket/cookit-backend/lib"
 	"github.com/Daniorocket/cookit-backend/models"
 	"github.com/gorilla/mux"
 	uuid "github.com/satori/go.uuid"
 )
 
 func (d *Handler) GetListOfCategories(w http.ResponseWriter, r *http.Request) {
+
 	page := r.URL.Query().Get("page")
 	limit := r.URL.Query().Get("limit")
 	p, err := strconv.Atoi(page)
@@ -40,55 +41,35 @@ func (d *Handler) GetListOfCategories(w http.ResponseWriter, r *http.Request) {
 	}, http.StatusOK, "success", "none")
 }
 func (d *Handler) CreateCategory(w http.ResponseWriter, r *http.Request) {
-	mr, err := r.MultipartReader()
+
+	cat := models.Category{}
+	file, err := lib.DecodeMultipartRequest(r, &cat)
 	if err != nil {
 		log.Println("Error:", err)
-		createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Failed to load image:")
+		createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Failed to decode multipart request")
+		return
+	}
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, file); err != nil {
+		log.Println("Error:", err)
+		createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Failed to read file")
 		return
 	}
 
-	cat := models.Category{}
-	for {
-		part, err := mr.NextPart()
-		if err == io.EOF { //End of multipart data
-			break
-		}
-		if err != nil {
-			log.Println("Error:", err)
-			createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Failed to read json:")
-			return
-		}
-		if part.FormName() == "file" {
-			buf := bytes.NewBuffer(nil)
-			if _, err := io.Copy(buf, part); err != nil {
-				log.Println("Error:", err)
-				createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Failed to read file:")
-				return
-			}
-			cat.File.EncodedURL = base64.StdEncoding.EncodeToString(buf.Bytes())
-			cat.File.Extension = path.Ext(part.FileName())
-			cat.ID = uuid.NewV4().String()
-			switch ext := cat.File.Extension; ext {
-			case ".jpg", ".JPG", ".png", ".PNG":
-			default:
-				log.Println("Error extension image")
-				createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Bad image extension:")
-				return
-			}
-		}
-		if part.FormName() == "json" {
-			jsonDecoder := json.NewDecoder(part)
-			err = jsonDecoder.Decode(&cat)
-			if err != nil {
-				log.Println("Error:", err)
-				createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Failed to decode json:")
-				return
-			}
-		}
+	cat.File.EncodedURL = base64.StdEncoding.EncodeToString(buf.Bytes())
+	cat.File.Extension = path.Ext(file.FileName())
+	cat.ID = uuid.NewV4().String()
+	switch ext := cat.File.Extension; ext {
+	case ".jpg", ".JPG", ".png", ".PNG":
+	default:
+		log.Println("Error:", err)
+		createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Bad file extension")
+		return
 	}
+
 	if err := models.CreateCategory(d.Client, d.DatabaseName, cat); err != nil {
 		log.Println("Error:", err)
-		createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Failed to create category:")
+		createApiResponse(w, nil, http.StatusInternalServerError, "failed", "Failed to create category")
 		return
 	}
 	createApiResponse(w, nil, http.StatusOK, "success", "none")
